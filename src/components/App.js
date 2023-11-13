a5a907fa90ce3e3382d5059ec08a4b274cc31437
Anup Kumar Jana <91030891+anupkumarjana@users.noreply.github.com>
2023-11-13 13:30:10 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.key}
+          content={noteItem.content}
+        />
+      ))}
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
